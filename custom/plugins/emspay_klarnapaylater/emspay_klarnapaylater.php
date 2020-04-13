01c2e3e994cb4ee0baac4ccdd685a2a62bfbd4a1
sorshireality <tutunikssa@gmail.com>
2020-04-13 20:07:19 +0300

    fix KP Later, add KP Now2

---
+++ custom/plugins/emspay_klarnapaylater/emspay_klarnapaylater.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace emspay_klarnapaylater;
+
+use Shopware\Components\Plugin;
+use Shopware\Components\Plugin\Context\ActivateContext;
+use Shopware\Components\Plugin\Context\DeactivateContext;
+use Shopware\Components\Plugin\Context\InstallContext;
+use Shopware\Components\Plugin\Context\UninstallContext;
+use Shopware\Models\Payment\Payment;
+
+class emspay_klarnapaylater extends Plugin
+{
+    /**
+     * @param InstallContext $context
+     */
+    public function install(InstallContext $context)
+    {
+        /** @var \Shopware\Components\Plugin\PaymentInstaller $installer */
+        $installer = $this->container->get('shopware.plugin_payment_installer');
+
+        $options = [
+            'name' => 'emspay_klarnapaylater',
+            'description' => 'EMS Online Klarna Pay Later',
+            'action' => 'EmsPayKlarnaPayLater',
+            'active' => 1,
+            'position' => 1,
+            'additionalDescription' =>
+                '<img src="custom/plugins/emspay_klarnapaylater/emspay_klarnapaylater.png"/>'
+                . '<div id="payment_desc">'
+                . '  Pay using EMS Online.'
+                . '</div>'
+        ];
+        $installer->createOrUpdate($context->getPlugin(), $options);
+    }
+    /**
+     * @param UninstallContext $context
+     */
+    public function uninstall(UninstallContext $context)
+    {
+        $this->setActiveFlag($context->getPlugin()->getPayments(), false);
+    }
+
+    /**
+     * @param DeactivateContext $context
+     */
+    public function deactivate(DeactivateContext $context)
+    {
+        $this->setActiveFlag($context->getPlugin()->getPayments(), false);
+    }
+
+    /**
+     * @param ActivateContext $context
+     */
+    public function activate(ActivateContext $context)
+    {
+        $this->setActiveFlag($context->getPlugin()->getPayments(), true);
+    }
+
+    /**
+     * @param Payment[] $payments
+     * @param $active bool
+     */
+    private function setActiveFlag($payments, $active)
+    {
+        $em = $this->container->get('models');
+
+        foreach ($payments as $payment) {
+            $payment->setActive($active);
+        }
+        $em->flush();
+    }
+}
