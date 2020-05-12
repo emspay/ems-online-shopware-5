41ae724238f8ff0ef2a6ac8d07cc294f5614cc37
sorshireality <tutunikssa@gmail.com>
2020-05-12 21:12:35 +0300

    reworked IPBlockSubscriber

--- custom/plugins/emspay/Subscriber/IPBlockSubscriber.php
+++ custom/plugins/emspay/Subscriber/IPBlockSubscriber.php
@@ -27,24 +27,18 @@ class IPBlockSubscriber implements SubscriberInterface
      */
     public function checkForDisplay(\Enlight_Event_EventArgs $args)
     {
+        $allowed_payments = array();
         $this->helper = Shopware()->Container()->get('emspay.helper');                                                                          //Create Helper
         $this->ems = $this->helper->getClient(Shopware()->Container()->get('shopware.plugin.cached_config_reader')->getByPluginName('emspay')); //Create EMS
 
-        foreach ($args->getSubject()->View()->getAssign()['sPayments'] as $payment) {
-            switch ($payment['name']) {
-                case 'emspay_klarnapaylater' :
-                $klarna_id = $payment['id'];
-                break;
-                case 'emspay_afterpay' :
-                $afterpay_id = $payment['id'];
+        foreach ($args->getSubject()->View()->getAssign()['sPayments'] as $key => $payment) {
+           $name = explode('emspay_',$payment['name'])[1];
+            if ($this->ipAddressValidation($name)) {
+                $allowed_payments[$key] = $payment;
             }
+
         }
-        if (!empty($klarna_id) && !$this->ipAddressValidation('klarna')) {
-            $this->cleanUp($args,$klarna_id);
-        }
-        if (!empty($afterpay_id) && !$this->ipAddressValidation('afterpay')) {
-            $this->cleanUp($args,$afterpay_id);
-        }
+        $args->getSubject()->View()->assign('sPayments',$allowed_payments);
     }
 
     /**
@@ -58,33 +52,5 @@ class IPBlockSubscriber implements SubscriberInterface
         $ip_list = array_map('trim', explode(",", $test_ip));
 
         return in_array($this->helper->getIpOfTheServer(), $ip_list);
-    }
-
-    /**
-     * Clean Up View for unavailable payment methods
-     * @param $args
-     * @param $payment_id
-     */
-    private function cleanUp($args,$payment_id){
-        
-        $view = $args->getSubject()->View();
-        $assigned = $view->getAssign();
-
-        //Clean up list of payment methods
-        unset($assigned['sPayments'][$payment_id]);
-        $view->assign('sPayments', $assigned['sPayments']);
-
-        //Clean up user-selected payment method
-        if ($assigned['sUserData']['additional']['payment']['id'] == $payment_id) {
-            unset($assigned['sUserData']['additional']['payment']);
-            $assigned['sUserData']['additional']['user']['paymentID'] = null;
-        }
-        $view->assign('sUserData', $assigned['sUserData']);
-
-        //Clean up Form Data
-        if ($assigned['sFormData']['payment'] == $payment_id) {
-            $assigned['sFormData']['payment'] = null;
-        }
-        $view->assign('sFormData', $assigned['sFormData']);
     }
 }
